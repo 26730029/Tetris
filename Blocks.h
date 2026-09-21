b26bdbeb0864f90459173e8386775e93a79354b9
Cloud-26730034 <26730034@ms.uit.edu.vn>
2026-09-21 08:48:07 +0700

    Revert "add class LBlock"
    This reverts commit e1f20d061b1dc417ef44683b33c92201f3be4976.

--- Blocks.h
+++ Blocks.h
@@ -19,48 +19,6 @@
 //   khong doi hinh dang).
 // ================================================================
 
-class Blocks
-{
-protected:
-    char shape[4][4];
-
-public:
-    Blocks()
-    {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                shape[i][j] = ' ';
-    }
-
-    // Destructor PHAI la virtual -- vi ta se xoa object thong qua
-    // con tro lop cha (Blocks*), neu khong co virtual o day, destructor
-    // cua lop con se khong duoc goi -> memory leak / undefined behavior.
-    virtual ~Blocks() {}
-
-    // Xoay ma tran 4x4 mot goc 90 do theo chieu kim dong ho.
-    // Day la hanh vi MAC DINH, dung chung cho hau het cac loai khoi
-    // (I, T, S, Z, J, L). Lop con nao can hanh vi khac (vi du OBlock)
-    // thi override lai ham nay.
-    virtual void rotate()
-    {
-        char temp[4][4];
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                temp[j][3 - i] = shape[i][j];
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                shape[i][j] = temp[i][j];
-    }
-
-    // Doc 1 o trong ma tran hinh dang (dung cho canMove/block2Board/...).
-    char get(int i, int j) const { return shape[i][j]; }
-
-    // Ghi 1 o trong ma tran hinh dang (dung khi can revert lai sau khi
-    // xoay ma vi tri moi khong hop le -- xem rotateCurrentBlock() trong
-    // main.cpp).
-    void set(int i, int j, char c) { shape[i][j] = c; }
-};
-
 class TBlock : public Blocks
 {
 public:
@@ -108,24 +66,46 @@ public:
     
 };
 
-class LBlock : public Blocks {
-public:
-    LBlock() 
-    {
-        char initialShape[4][4] = {
-            {' ', ' ', ' ', ' '},
-            {' ', ' ', 'L', ' '},
-            {'L', 'L', 'L', ' '},
-            {' ', ' ', ' ', ' '}
-        };
+class Blocks
+{
+protected:
+    char shape[4][4];
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) 
-            {
-                shape[i][j] = initialShape[i][j];
-            }
-        }
+public:
+    Blocks()
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                shape[i][j] = ' ';
     }
+
+    // Destructor PHAI la virtual -- vi ta se xoa object thong qua
+    // con tro lop cha (Blocks*), neu khong co virtual o day, destructor
+    // cua lop con se khong duoc goi -> memory leak / undefined behavior.
+    virtual ~Blocks() {}
+
+    // Xoay ma tran 4x4 mot goc 90 do theo chieu kim dong ho.
+    // Day la hanh vi MAC DINH, dung chung cho hau het cac loai khoi
+    // (I, T, S, Z, J, L). Lop con nao can hanh vi khac (vi du OBlock)
+    // thi override lai ham nay.
+    virtual void rotate()
+    {
+        char temp[4][4];
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                temp[j][3 - i] = shape[i][j];
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                shape[i][j] = temp[i][j];
+    }
+
+    // Doc 1 o trong ma tran hinh dang (dung cho canMove/block2Board/...).
+    char get(int i, int j) const { return shape[i][j]; }
+
+    // Ghi 1 o trong ma tran hinh dang (dung khi can revert lai sau khi
+    // xoay ma vi tri moi khong hop le -- xem rotateCurrentBlock() trong
+    // main.cpp).
+    void set(int i, int j, char c) { shape[i][j] = c; }
 };
 
 #endif
